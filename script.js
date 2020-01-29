ff770bd0a515caa01633b1c8f207e8fdfab9a017
plovislov <plovislov@gmail.com>
2020-01-29 10:51:26 +0300

    Logical operators lession

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-'use strict';
+// Write an “if” condition to check that age is between 14 and 90 inclusively.
+//
+// “Inclusively” means that age can reach the edges 14 or 90.
 
-alert('hello world!');
+let age = 13;
+
+if (age >= 14 && age <= 90) {
+    console.log('it\'s in range');
+}
+
+// Write an if condition to check that age is NOT between 14 and 90 inclusively.
+//
+//     Create two variants: the first one using NOT !, the second one – without it.
+
+if (age < 14 || age > 90) {
+    console.log('ok. out of range');
+}
+
+if (!(age >= 14 && age <= 90)) {
+    console.log('ok. out of range (With NOT operator)');
+}
