7f7ce01026f376dfd0ac2a3cb87fd9e1d1d27760
plovislov <plovislov@gmail.com>
2020-01-30 10:51:20 +0300

    Loops lession

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-// Write the code which asks for a login with prompt.
-//
-//     If the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled”, if it’s another string – then show “I don’t know you”.
-//
-// The password is checked as follows:
-//
-// If it equals “TheMaster”, then show “Welcome!”,
-// Another string – show “Wrong password”,
-// For an empty string or cancelled input, show “Canceled”
+let i = 0;
+while (i++ < 5) {
+    console.log( i );
+}
 
-let login = prompt('Enter login', '');
-let password = '';
-
-if (login === 'Admin') {
-    password = prompt('Enter password', '');
-
-    if (password === 'TheMaster') {
-        alert('Welcome!');
-    } else if (password === null || password === '') {
-        alert('Canceled');
-    } else {
-        alert('Wrong password');
-    }
-
-} else if (login === null || login === '') {
-    alert('Canceled');
-} else {
-    alert('I don\'t know you');
+for (; i < 5; i++) {
+    console.log( i );
 }
