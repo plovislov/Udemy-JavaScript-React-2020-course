a82775a72a1c25c70ebd2912339bcd82d0b20fde
plovislov <plovislov@gmail.com>
2020-02-07 10:15:30 +0300

    Constructors lession

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function sumSalaries(obj) {
-  let result = 0;
-  for (key in obj) {
-    result += obj[key];
-  }
-  return result;
+function Calculator() {
+  this.numOne = 0;
+  this.numTwo = 0;
+
+  this.read = function() {
+    this.numOne = +prompt('Enter first number', '0');
+    this.numTwo = +prompt('Enter second number', '0');
+  };
+
+  this.sum = function() {
+    return this.numOne + this.numTwo;
+  };
+
+  this.mul = function() {
+    return this.numOne * this.numTwo;
+  };
 }
+
+let calc = new Calculator();
+
+calc.read();
+
+console.log(`sum =: ${calc.sum()}`);
+console.log(`mul =: ${calc.mul()}`);
