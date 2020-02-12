da198abab74618fedc8b6c48a322446ed7727fa9
plovislov <plovislov@gmail.com>
2020-02-12 12:53:03 +0300

    Lession Arrays

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-// // Write a function filterRange(arr, a, b) that gets an array arr, looks for elements between a and b in it and returns an array of them.
-//
-function filterRange(arr, a, b) {
-  return arr.filter(item => (item >= a && item <= b));
-}
+function Calculator() {
+  this.methods = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+  };
 
+  this.calculate = function(str) {
+    let split = str.split(' '),
+        a = +split[0],
+        op = split[1],
+        b = +split[2];
 
-// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
-
-function filterRangeInPlace(arr, a, b) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let val = arr[i];
-
-    if (val <= a || val >= b) {
-      arr.splice(i, 1);
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
     }
 
-  }
+    return this.methods[op](a, b);
+  };
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func;
+  };
 }
 
-//Sort in decreasing order
+let calc = new Calculator();
 
-let arr = [5, 2, 1, -10, 8];
-
-arr.sort(function(a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
-  if (a === b) return 0;
-});
-arr.reverse();
-
-// We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified
-
-function copySorted(arr) {
-  let copy = arr.slice();
-
-  copy.sort();
-
-  return copy;
-}
-
-let arr = ['HTML', 'JavaScript', 'CSS'];
-let sorted = copySorted(arr);
-console.log(sorted); // CSS, HTML, JavaScript
-console.log(arr); // HTML, JavaScript, CSS (no changes)
+console.log(calc.calculate('3 + 7'));
+console.log(calc.calculate('10 - 7'));
+calc.addMethod('/', (a, b) => a * b);
