1511c9ee75fc36d255a5e0ef71157c7ee47c4f4e
plovislov <plovislov@gmail.com>
2020-02-12 12:16:36 +0300

    Lession Arrays

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
-
-function camelize(str) {
-  return str.split('-').map(function(item, index) {
-    if (index === 0) {
-      return item;
-    } else {
-      return item[0].toUpperCase() + item.slice(1);
-    }
-  }).join('');
-
+// // Write a function filterRange(arr, a, b) that gets an array arr, looks for elements between a and b in it and returns an array of them.
+//
+function filterRange(arr, a, b) {
+  return arr.filter(item => (item >= a && item <= b));
 }
+
+
+// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let val = arr[i];
+
+    if (val <= a || val >= b) {
+      arr.splice(i, 1);
+    }
+
+  }
+}
+
+//Sort in decreasing order
+
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort(function(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+  if (a === b) return 0;
+});
+arr.reverse();
+
+// We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified
+
+function copySorted(arr) {
+  let copy = arr.slice();
+
+  copy.sort();
+
+  return copy;
+}
+
+let arr = ['HTML', 'JavaScript', 'CSS'];
+let sorted = copySorted(arr);
+console.log(sorted); // CSS, HTML, JavaScript
+console.log(arr); // HTML, JavaScript, CSS (no changes)
