ec052327caa73d7ddd63470e5419d32305a68cff
plovislov <57401964+plovislov@users.noreply.github.com>
2020-02-18 13:35:58 +0300

    Lession Map & Set

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-let john = {name: 'John', surname: 'Smith', id: 1};
-let pete = {name: 'Pete', surname: 'Hunt', id: 2};
-let mary = {name: 'Mary', surname: 'Key', id: 3};
+function unique(arr) {
+  let set = new Set(arr);
+  return Array.from(set);
+}
 
-let users = [john, pete, mary];
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
 
-let usersMapped = users.map(function(item) {
-  return {
-    fullName: `${item.name} ${item.surname}`,
-    id: item.id,
-  };
-});
-
-/*
-usersMapped = [
-  { fullName: "John Smith", id: 1 },
-  { fullName: "Pete Hunt", id: 2 },
-  { fullName: "Mary Key", id: 3 }
-]
-*/
-console.log(usersMapped);
-
-console.log(usersMapped[0].id); // 1
-console.log(usersMapped[0].fullName); // John Smith
+console.log(unique(values));
