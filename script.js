fa6802b0787830fdf75554bcc890749979357636
plovislov <plovislov@gmail.com>
2020-02-24 12:21:48 +0300

    Lession recursion

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-let someDay = new Date('2012-02-20T03:20:00');
-console.log(someDay);
+function sumTo(n) {
+    let result = 0;
 
-function getWeekDay(date) {
-    let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
-    return days[date.getDay()];
+    for (let i = 0; i <= n; i++) {
+        result += i;
+    }
+    return result;
 }
 
-let now = new Date();
-
-console.log(getWeekDay(now));
-
-function getSecondsTillTomorrow() {
-    let now = new Date();
-    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
-    let diff = tomorrow - now;
-    console.log(Math.round(diff / 1000));
+function sumTo(n) {
+    if (n === 1) {
+        return n;
+    } else {
+        return n + sumTo(n-1);
+    }
 }
 
-getSecondsTillTomorrow();
+function sumTo(n) {
+    return n * (1 + n) / 2;
+}
+
+
+console.log(sumTo(1)); // 1
+console.log(sumTo(2)); //3
+console.log(sumTo(3)); // 6
+console.log(sumTo(4)); // 10
+console.log(sumTo(100)); // 5050
