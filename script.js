d505f301e6edb1adea76a68b6de83f3a4eff08c0
plovislov <plovislov@gmail.com>
2020-02-28 12:16:35 +0300

    Lession variable scope

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-function inBetween(a, b) {
-    return function (x) {
-        return x >= a && x <= b;
+let users = [
+    {name: "John", age: 20, surname: "Johnson"},
+    {name: "Pete", age: 18, surname: "Peterson"},
+    {name: "Ann", age: 19, surname: "Hathaway"}
+];
 
-    }
+function byField(field) {
+    return (a, b) => a[field] > b[field] ? 1 : -1;
 }
 
-function inArray(arr) {
-    return function (x) {
-        return arr.includes(x);
-    }
-}
+let byName = users.sort(byField('name'));
+console.log(byName);
 
-
-/* .. your code for inBetween and inArray */
-let arr = [1, 2, 3, 4, 5, 6, 7];
-
-console.log(arr.filter(inBetween(3, 6))); // 3,4,5,6
-
-console.log(arr.filter(inArray([1, 2, 10]))); // 1,2
+let byAge = users.sort(byField('age'));
+console.log(byAge);
