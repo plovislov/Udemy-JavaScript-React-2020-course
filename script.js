1759533a5e61063bacd10d4dab567b979ee20ae9
plovislov <57401964+plovislov@users.noreply.github.com>
2020-03-24 15:41:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ console.log( speedy.stomach ); // apple
 
 // У этого хомяка тоже есть еда. Почему? Исправьте
 console.log( lazy.stomach ); // apple
+
+
+//task 2 
+
+function f() {
+  console.log("Hello!");
+}
+
+Function.prototype.defer = function (ms) {
+  return setTimeout(()=>this.call(arguments), ms);
+}
+
+f.defer(1000); // выведет "Hello!" через 1 секунду
