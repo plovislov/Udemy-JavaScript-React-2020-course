7c86cbe1e45a955c31782c0e323e9684dcdcc99a
plovislov <plovislov@gmail.com>
2021-01-20 21:13:04 +0300

    88. Знакомство с JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const elem = (
+    <div>
+
+
+    </div>
+);
+
+const Header = () => {
+  return <h2>Hello world</h2>
+}
+
+const Field = () => {
+  const holder = 'Enter here';
+
+  const styledField = {
+    width: '300px'
+  }
+
+  return <input
+      style={styledField}
+      type="text"
+      placeholder={holder}
+      autoComplete=''
+      className='first'
+      htmlFor=''/>
+}
+
+const Btn = () => {
+  const text = 'Log in';
+  const logged = false;
+
+  const res = () => {
+    return 'Log in';
+  }
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
+const App = () => {
+  return (
+      <div>
+        <Header/>
+        <Field/>
+        <Btn/>
+      </div>
+  )
+}
+
+ReactDOM.render(<App/>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
