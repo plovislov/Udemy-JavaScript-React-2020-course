ffcadeba201d299a7fb5438efaf5e233418c6e13
plovislov <plovislov@gmail.com>
2021-01-22 18:07:53 +0300

    new project twit

--- twit/src/index.js
+++ twit/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/app/';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 26
+    }
+    this.nextYear = this.nextYear.bind(this);
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    const {years} = this.state;
+    return (
+        <>
+          <h1>My name is {name}, surname - {surname}, years = {years}</h1>
+          <a href={link}>My profile</a>
+        </>
+    )
+  }
+}
+
+const All = () => {
+  return (
+      <>
+        <WhoAmI name='John' surname='Smith' link='facebook.com'/>
+        <WhoAmI name='Alex' surname='Smith' link='facebook.com'/>
+        <WhoAmI name='Kin' surname='Smith' link='facebook.com'/>
+      </>
+  );
+}
+
+ReactDOM.render(<App/>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
